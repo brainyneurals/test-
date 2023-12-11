1453d9ed882faea503ccc4b6524e2c753e5dd011
brainyneurals <153489505+brainyneurals@users.noreply.github.com>
2023-12-11 14:09:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st 
 
+import streamlit as st
+
+# Create a Streamlit sidebar to get user input
 st.sidebar.header("User Input")
 
 # Get user input for a text area
@@ -8,4 +11,10 @@ user_input_data = st.sidebar.text_area("Enter your data here:")
 # Display the user input
 st.write("User Input Data:")
 st.write(user_input_data)
+
+# Save the user input data to a text file
+if st.sidebar.button("Save Data"):
+    with open("user_input_data.txt", "w") as file:
+        file.write(user_input_data)
+    st.sidebar.success("Data saved successfully!")
 
