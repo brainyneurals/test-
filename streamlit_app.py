2cc4bc2190f485b23d5a97e8107e2110d971b658
brainyneurals <153489505+brainyneurals@users.noreply.github.com>
2023-12-11 13:20:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit as st 
 
-st.title("Hello World")
+st.sidebar.header("User Input")
+
+# Get user input for a text area
+user_input_data = st.sidebar.text_area("Enter your data here:")
+
+# Display the user input
+st.write("User Input Data:")
+st.write(user_input_data)
 
